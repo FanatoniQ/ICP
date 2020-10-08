c15f0a041e111705eb940387c2383cf5a7142db2
Sabrina Meng <sabmeng99@gmail.com>
2020-10-08 14:57:12 +0200

    [Testsuite] Added some pretty printing

--- tests/test.py
+++ tests/test.py
@@ -7,6 +7,31 @@ import subprocess
 import multiprocessing
 import time
 
+HEADER = '\033[95m'
+OKBLUE = '\033[94m'
+OKGREEN = '\033[92m'
+WARNING = '\033[93m'
+FAIL = '\033[91m'
+ENDC = '\033[0m'
+BOLD = '\033[1m'
+UNDERLINE = '\033[4m'
+
+def print_test(TEST):
+    """Pretty print for testsuite start"""
+    print(WARNING + "\n  ===================")
+    print(" /                   \\")
+    print("       " + TEST )
+    print(" \\                   /")
+    print("  ===================" + ENDC)
+
+def end_print(total, success, fails):
+    """Pretty print for testsuite results"""
+    print("\nTotal:", total)
+    print(OKGREEN + "Success:" + ENDC, success)
+    print(FAIL + "Fails:" + ENDC , fails)
+
+#-------------------------------------------------
+
 def mean(P):
     P = pd.read_csv(P)
     P = np.array(P).T
@@ -20,6 +45,11 @@ def dotproduct(P, Q):
     return P.dot(Q)
 
 def exec(fn, *args):
+    print_test('TESTSUITE')
+
+    nbtests = 0
+    success = 0
+
     print(*args)
     expected = globals()[fn](*args)
     executable = "./testlibalg{}".format(fn)
@@ -30,6 +60,8 @@ def exec(fn, *args):
     #input(expected)
     assert(np.allclose(expected, R))
     print("Success !")
+    success += 1; nbtests += 1
+    end_print(1, success, nbtests - success)
 
 if __name__ == "__main__":
     fn = sys.argv[-1]
