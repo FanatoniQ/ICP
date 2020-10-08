6f6134e7753eacb8603c0da48f455f24d3395518
Guillaume Blassel <unterfox@gmail.com>
2020-10-08 21:54:43 +0200

    libalg: csv printing function

---
+++ src/testlibalg/testlibalgcsvprint.cpp
@@ -0,0 +1,40 @@
+#include <err.h>
+
+#include <float.h>
+#include <stdlib.h>
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+#include "libCSV/csv.hpp"
+#include "libalg/alg.hpp"
+#include "libalg/mean.hpp"
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+        errx(1, "Usage: ./testlibalgcsvprint file1");
+    size_t nbaxis, nbpoints;
+    std::string f1Header{};
+    std::ifstream file1(argv[1]);
+    double *m = readCSV(file1, f1Header, &nbaxis, &nbpoints);
+
+    // std::cout << std::setprecision(15);
+    std::cerr << "nbaxis: " << nbaxis << " nbpoints: " << nbpoints << std::endl;
+    for (size_t i = 0; i < nbpoints / nbaxis; ++i)
+    {
+        for (size_t j = 0; j < nbaxis; ++j)
+        {
+            std::cout << m[i*nbaxis + j];
+            if (j != nbaxis - 1)
+                std::cout << ',';
+            else
+                std::cout << std::endl;
+            
+        }
+    }
+
+    free(m);
+    return 0;
+}
