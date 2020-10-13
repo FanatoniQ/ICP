65fca4bcb5a70709cb9e86b3a72fe592840dd828
Tom Mechineau <tom.mechineau@gmail.com>
2020-10-13 19:25:28 +0200

    Add multiple operators and more to come

--- include/libalg/matrix.hpp
+++ include/libalg/matrix.hpp
@@ -12,14 +12,34 @@ public:
     // Constructors
     Matrix();
     Matrix(double *array, int dim0, int dim1);
+    /**
+     * Initialize matrix of dim0*dim1 with init value
+     * Throws std::bad_alloc on failed malloc
+     * @param dim0
+     * @param dim1
+     * @param init
+     */
+    Matrix(size_t dim0, size_t dim1, double init);
+
+    /**
+     * Free the ressources
+     */
+    virtual ~Matrix();
 
     // Getters and setters
+    // TODO remove
     double *getArray() const;
-    void setArray(double *array);
+
+    /**
+     * Replace the array pointer and returns the previous one
+     * @param array_
+     * @param dim0_
+     * @param dim1_
+     * @return Matrix::array
+    */
+    double *setArray(double *array, size_t dim0_, size_t dim1_);
     size_t getDim0() const;
-    void setDim0(size_t dim0);
     size_t getDim1() const;
-    void setDim1(size_t dim1);
 
     // Output
     friend std::ostream &operator<<(std::ostream &os, const Matrix &matrix);
@@ -27,6 +47,32 @@ public:
     // Operations
     bool operator==(const Matrix &rhs) const;
     bool operator!=(const Matrix &rhs) const;
-    std::vector<double> mean_axis(size_t dim)
+    std::vector<double> mean_axis(size_t dim);
+
+    // Operator overloading, for "standard" mathematical matrix operations
+    Matrix& operator=(const Matrix& rhs);
+
+    // Matrix mathematical operations
+    Matrix operator+(const Matrix& rhs);
+    Matrix& operator+=(const Matrix& rhs);
+    Matrix operator-(const Matrix& rhs);
+    Matrix& operator-=(const Matrix& rhs);
+    Matrix operator*(const Matrix& rhs);
+    Matrix& operator*=(const Matrix& rhs);
+    Matrix transpose();
+
+    // Matrix/scalar operations
+    Matrix operator+(const double& rhs);
+    Matrix operator-(const double& rhs);
+    Matrix operator*(const double& rhs);
+    Matrix operator/(const double& rhs);
+
+    // Matrix/vector operations
+    std::vector<double> operator*(const std::vector<double>& rhs);
+    std::vector<double> diag_vec();
+
+    // Access the individual elements
+    double& operator()(const unsigned& row, const unsigned& col);
+    const double& operator()(const unsigned& row, const unsigned& col) const;
 
 };
--- src/libalg/matrix.cpp
+++ src/libalg/matrix.cpp
@@ -1,21 +1,46 @@
 #include "libalg/matrix.hpp"
+#include <stdexcept>
 
 Matrix::Matrix(): array(nullptr), dim0(0), dim1(0) {}
 
 Matrix::Matrix(double* array, int dim0, int dim1) : array(array), dim0(dim0), dim1(dim1) {}
 
+Matrix::Matrix(size_t dim0, size_t dim1, double init): dim0(dim0), dim1(dim1) {
+    Matrix::array = (double*) malloc(dim0 * dim1 * sizeof(double));
+    if (Matrix::array == nullptr)
+        throw std::bad_alloc();
+    for (unsigned i=0; i<dim0; ++i) {
+        for (unsigned j=0; j<dim1; ++j) {
+            Matrix::array[i * dim1 + j] = init;
+        }
+    }
+}
+
 std::vector<double> Matrix::mean_axis(size_t dim)
 {
     size_t i, j;
-    std::vector<double> r(5);
-
-    for (i = 0; i < nb_axis; ++i)
-    {
-        for (j = 0; j < nb_points; ++j)
+    std::vector<double> r;
+    if (dim == 0) {
+        r.resize(dim1, 0);
+        for (i = 0; i < dim1; ++i)
         {
-            r[i] += m[i * nb_points + j];
+            for (j = 0; j < dim0; ++j)
+            {
+                r[i] += array[j * dim1 + i];
+            }
+            r[i] /= dim0;
         }
-        r[i] /= nb_points;
+    }
+    else {
+        r.resize(dim0, 0);
+        for (i = 0; i < dim0; ++i)
+        {
+            for (j = 0; j < dim1; ++j)
+            {
+                r[i] += array[i * dim1 + j];
+            }
+            r[i] /= dim1;
+        }
     }
     return r;
 }
@@ -41,23 +66,82 @@ double *Matrix::getArray() const {
     return array;
 }
 
-void Matrix::setArray(double *array_) {
+double *Matrix::setArray(double *array_, size_t dim0_, size_t dim1_) {
+    double *tmp = Matrix::array;
     Matrix::array = array_;
+    Matrix::dim0 = dim0_;
+    Matrix::dim1 = dim1_;
+    return tmp;
 }
 
 size_t Matrix::getDim0() const {
     return dim0;
 }
 
-void Matrix::setDim0(size_t dim0_) {
-    Matrix::dim0 = dim0_;
-}
-
 size_t Matrix::getDim1() const {
     return dim1;
 }
 
-void Matrix::setDim1(size_t dim1_) {
-    Matrix::dim1 = dim1_;
+Matrix::~Matrix() {
+    free(array);
 }
+
+Matrix &Matrix::operator=(const Matrix &rhs) {
+    if (&rhs == this)
+        return *this;
+
+    unsigned new_rows = rhs.getDim0();
+    unsigned new_cols = rhs.getDim1();
+
+    // Allocate before freeing in case of error
+    auto *new_array = (double*)malloc(new_rows * new_cols * sizeof(double));
+    if (new_array == nullptr) {
+        throw;
+    }
+
+    // Free old pointer
+    free(array);
+    array = new_array;
+    dim0 = new_rows;
+    dim1 = new_cols;
+
+    for (unsigned i=0; i<dim0; ++i) {
+        for (unsigned j=0; j<dim1; ++j) {
+            (*this)(i,j) = rhs(i, j);
+        }
+    }
+    return *this;
+}
+
+Matrix Matrix::operator+(const Matrix &rhs) {
+    Matrix result(dim0, dim1, 0.0);
+
+    for (unsigned i=0; i<dim0; ++i) {
+        for (unsigned j=0; j<dim1; ++j) {
+            result(i,j) = (*this)(i,j) + rhs(i,j);
+        }
+    }
+    return result;
+}
+
+Matrix &Matrix::operator+=(const Matrix &rhs) {
+
+    for (unsigned i=0; i<rhs.getDim0(); ++i) {
+        for (unsigned j=0; j<rhs.getDim1(); ++j) {
+            (*this)(i,j) += rhs(i,j);
+        }
+    }
+    return *this;
+}
+
+// FIXME be able to set with the operator
+double &Matrix::operator()(const unsigned int &row, const unsigned int &col) {
+    return array[row * dim0 + col];
+}
+
+const double &Matrix::operator()(const unsigned int &row, const unsigned int &col) const {
+    return array[row * dim0 + col];
+}
+
+
 
