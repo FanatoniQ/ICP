cb4e9845125981ca6d58959c03aa4015e0486d7f
Guillaume Blassel <unterfox@gmail.com>
2020-10-14 14:05:07 +0200

    icp : adding basis of cross_variance, still to be tested

--- src/cpu/icp.cpp
+++ src/cpu/icp.cpp
@@ -1,11 +1,13 @@
 #include <vector>
 #include <limits>
 #include <tuple>
+#include <iostream>
 
 #include "libalg/alg.hpp"
 #include "cpu/icp.hpp"
 
-std::vector<std::tuple<size_t, int>> get_correspondence_indices(double *P, double *Q, size_t P_r, size_t P_c, size_t Q_r, size_t Q_c)
+std::vector<std::tuple<size_t, int>> get_correspondence_indices(double *P, double *Q,
+    size_t P_r, size_t P_c, size_t Q_r, size_t Q_c)
 {
     std::vector<std::tuple<size_t, int>> correspondances = {};
     for (size_t i = 0; i < P_r; i++)
@@ -31,20 +33,39 @@ std::vector<std::tuple<size_t, int>> get_correspondence_indices(double *P, doubl
     return correspondances;
 }
 
-/*
-std::tuple<double, double> compute_cross_variance(double *P, double *Q, std::vector<std::tuple<size_t, int>> correspondences, size_t P_r, size_t P_c, size_t Q_r, size_t Q_c)
+double default_kernel(double a)
 {
-    double *cov = (double*)calloc(4, sizeof(double));
+    if (a == 1) //fucking W_error
+        return 1;
+    else
+        return 1;
+} 
+
+std::tuple<double**, std::vector<double>> compute_cross_variance(double *P, double *Q,
+    std::vector<std::tuple<size_t, int>> correspondences, size_t P_r, size_t P_c,
+    size_t Q_r, size_t Q_c, double (*kernel)(double a)) //set default function to lambda function??
+{
+    if (kernel == nullptr)
+        kernel = &default_kernel;
+    double **cov = (double**)calloc(4, sizeof(double));
     std::vector<double> exclude_indices = {};
     for (auto tup : correspondences)
     {
         auto i = std::get<0>(tup);
         auto j = std::get<1>(tup);
-        double *transposed_P = transpose(P, P_r, P_c);
+        double *transposed_P = transpose(P, P_r, P_c); 
         double *transposed_Q = transpose(Q, Q_r, Q_c);
         double *q_point = transposed_Q + j*Q_c;
         double *p_point = transposed_P + i*P_c;
-        auto weight = diff()
+        double weight = kernel(*p_point - *q_point);
+        std::cout << weight << std::endl;
+        if (weight < 0.01)
+            exclude_indices.push_back(i);
+        double **doted_points = nullptr;
+        dot_product(doted_points, q_point, transpose(P, P_r, P_c), Q_r, Q_c, P_c, P_r); //dim of Q_r * P_r
+        double **weighted_points = nullptr; //multiply by the weight 
+        element_wise_op(weighted_points, &weight, *doted_points, 1, 1, Q_r, P_r, mult);
+        element_wise_op(cov, *cov, *weighted_points, 2, 2, Q_r, P_r, add);
     }
+    return std::make_tuple(cov, exclude_indices);
 }
-*/
