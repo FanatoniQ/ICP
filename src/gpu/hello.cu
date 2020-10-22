125d09b358937ac72f40a0c4ec65fe7328abac29
Geoffrey Bossut <geoffrey.bossut@r01p12.sm14.sm.cri.epita.net>
2020-10-22 21:22:38 +0200

    GPU: sum adding line sum with tree reduction and shared memory

--- src/gpu/hello.cu
+++ src/gpu/hello.cu
@@ -6,10 +6,19 @@
 #include "libalg/CPUMatrix.hpp"
 #include "libalg/alg.hpp"
 #include "libalg/print.hpp"
+#include "error.hpp"
+
+#define cudaCheckError() {                                                                       \
+        cudaError_t e=cudaGetLastError();                                                        \
+        if(e!=cudaSuccess) {                                                                     \
+            printf("Cuda failure %s:%d: '%s'\n",__FILE__,__LINE__,cudaGetErrorString(e));        \
+            exit(EXIT_FAILURE);                                                                  \
+        }                                                                                        \
+    }
 
 __global__ void print_kernel()
 {
-    printf("Hello from block %d, thread %d\n", blockIdx.x, threadIdx.x);
+    printf("Hello from block %d, thread %d\n", blockIdx.y * 10 + blockIdx.x, threadIdx.x);
 }
 
 // TODO: REMOVE ME since useless
@@ -56,18 +65,16 @@ __device__ __host__ double get_line_mean(const double *line, int nbvals)
  ** \param d_A the device pointer
  ** \param d_sumA the device sum pointer used to store sum results
  ** \param pitch the pitch in bytes
- ** \param nbvals the number of values in a line
+ ** \param width the number of values in a line
+ ** \param height the number of lines
  **/
-__global__ void dumb_sum_kernel(const char *d_A, double *d_sumA, int pitch, int nbvals)
+__global__ void dumb_sum_kernel(const char *d_A, double *d_sumA, int pitch, int width, int height)
 {
-    //int j;
     int idx = threadIdx.x;
-    //double sum = 0;
+    if (idx >= height)
+       return;
     double *line = (double *)(d_A + idx * pitch);
-    //for (j = 0; j < nbvals; ++j)
-    //    sum += line[j];
-    //d_sumA[idx] = sum;
-    d_sumA[idx] = get_line_sum(line, nbvals);
+    d_sumA[idx] = get_line_sum(line, width);
 }
 
 /**
@@ -79,13 +86,45 @@ __global__ void dumb_sum_kernel(const char *d_A, double *d_sumA, int pitch, int 
  ** \param d_A the device pointer
  ** \param d_meanA the device mean pointer used to store mean results
  ** \param pitch the pitch in bytes
- ** \param nbvals the number of values in a line
+ ** \param width the number of values in a line
+ ** \param height the number of lines
  **/
-__global__ void dumb_mean_kernel(const char *d_A, double *d_meanA, int pitch, int nbvals)
+__global__ void dumb_mean_kernel(const char *d_A, double *d_meanA, int pitch, int width, int height)
 {
     int idx = threadIdx.x;
+    if (idx >= height)
+        return;
     double *line = (double *)(d_A + idx * pitch);
-    d_meanA[idx] = get_line_mean(line, nbvals); //get_line_sum(line, nbvals) / nbvals;
+    d_meanA[idx] = get_line_mean(line, width);
+}
+
+__global__ void tree_reduce_sum_kernel(const double *d_A, double *d_sumA, int pitch, int width, int height, int reducepitch)
+{
+    extern __shared__ double s_data[]; // s_data is of size blockDim.x
+    int threadid = threadIdx.x; // thread id in the block
+    int lineid = blockIdx.y; // line
+    int dataid = blockIdx.x * blockDim.x + threadIdx.x; // column
+    if (dataid >= width || lineid >= height) {
+	s_data[threadid] = 0; // prevent other threads from adding uninit values
+        return;
+    }
+    double *d_Aline = (double *)((char *)d_A + lineid * pitch);
+    // each thread copies to shared memory
+    s_data[threadid] = d_Aline[dataid];
+    __syncthreads();
+    // each thread will reduce with one other shared data element in the middle right part of s_data
+    for (size_t stride = blockDim.x / 2; stride > 0; stride = stride >> 1)
+    {
+        if (threadid < stride) // a lot of threads are idle...
+             s_data[threadid] += s_data[threadid + stride];
+	__syncthreads();
+    }
+    //printf("Reduce Index: %d\n", blockIdx.x + reducepitch * lineid);
+    double *d_sumAline = (double *)((char *)d_sumA + lineid * reducepitch);
+    if (threadid == 0)
+        d_sumAline[blockIdx.x] = s_data[0];
+    //d_sumA[blockIdx.x + reducepitch * lineid] = s_data[0]; // we store at pos x,y the partial mean
+    //d_sumA[blockIdx.x + (height / blockDim.x) * lineid] = s_data[0]; // we store at pos x,y the partial mean
 }
 
 int main(int argc, char **argv)
@@ -103,7 +142,7 @@ int main(int argc, char **argv)
     print_matrix(std::cerr, h_p, nbcols, nblines);
 
     auto P = CPUMatrix(h_pT, nbcols, nblines);
-    std::cerr << "CPU Means: " << std::endl << P.mean(1) << P.mean(0) << std::endl;
+    std::cerr << "CPU Sums: " << std::endl << P.sum(1) << P.sum(0) << std::endl;
 
     //print_kernel<<<2, 3>>>();
 
@@ -111,31 +150,60 @@ int main(int argc, char **argv)
     double *d_pT;
     size_t pitch;
     size_t width = nblines, height = nbcols;
-    cudaMallocPitch(&d_pT, &pitch, width * sizeof(double), height * sizeof(double)); // TODO: error checking
-    cudaMemcpy2D(d_pT, pitch, h_pT, width * sizeof(double), width * sizeof(double), height, cudaMemcpyHostToDevice); // TODO: error checking
+    cudaMallocPitch(&d_pT, &pitch, width * sizeof(double), height * sizeof(double));
+    cudaCheckError();
+    cudaMemcpy2D(d_pT, pitch, h_pT, width * sizeof(double), width * sizeof(double), height, cudaMemcpyHostToDevice);
+    cudaCheckError();
     double *d_mean;
-    cudaMalloc(&d_mean, height * sizeof(double)); // TODO: error checking
-
+    //cudaMalloc(&d_mean, height * sizeof(double));
+    
     // kernel launching
-    //print_matrix_kernel<<<1, height>>>((char *)d_pT, pitch, width);
-    //dumb_sum_kernel<<<1, height>>>((char*)d_pT, d_mean, pitch, width);
-    dumb_mean_kernel<<<1, height>>>((char*)d_pT, d_mean, pitch, width);
+    //int blocks = 2;
+    //int threads = std::ceil((float)height / blocks); // max is 1024 threads per blocks !
+    //runtime_assert(blocks * threads >= height, "Invalid Grid Shape !");
+    int threads = 4; // TODO: change this
+    int nbblocksPerLine = std::ceil((float)width / threads); // each block line treats one line sum
+    dim3 blocks(nbblocksPerLine, height); // we have lines line of nbblocksPerLine
+    size_t reducepitch;
+    cudaMallocPitch(&d_mean, &reducepitch, nbblocksPerLine * sizeof(double), height);
+    cudaCheckError();
+    cudaMemset2D(d_mean, reducepitch, 0, nbblocksPerLine * sizeof(double), height);
+    cudaCheckError();
+    std::cerr << "nbthreads: " << threads << " nbblocksPerLine: " << blocks.x << " nbLines: " << blocks.y << std::endl;
+    //print_kernel<<<blocks, threads>>>();
+    //print_matrix_kernel<<<blocks, threads>>>((char *)d_pT, pitch, width);
+    //dumb_sum_kernel<<<blocks, threads>>>((char*)d_pT, d_mean, pitch, width);
+    //dumb_mean_kernel<<<blocks, threads>>>((char*)d_pT, d_mean, pitch, width, height);
+    tree_reduce_sum_kernel<<<blocks, threads, threads>>>(d_pT, d_mean, pitch, width, height, reducepitch); // FIXME: illegal mem access
     cudaDeviceSynchronize();
+    cudaCheckError();
 
     // copy back to host memory
     double *h_mean;
-    h_mean = (double*)malloc(height * sizeof(double));
-    if (h_mean == nullptr)
-        exit(1);
-    cudaMemcpy(h_mean, d_mean, height * sizeof(double), cudaMemcpyDeviceToHost);
+    h_mean = (double*)malloc(height * reducepitch);
+    runtime_assert(h_mean != nullptr, "Alloc error !");
+    cudaMemcpy(h_mean, d_mean, height * reducepitch, cudaMemcpyDeviceToHost);
+    cudaCheckError();
 
     std::cerr << "GPU Mean : " << std::endl;
     for (size_t i = 0; i < height; ++i)
-        std::cerr << h_mean[i] << "\t";
+    {
+        double v = 0;
+	double *line = (double *)((char *)h_mean + i * reducepitch);
+        for (size_t j = 0; j < nbblocksPerLine; ++j)
+	{
+            std::cerr << "[]: " << line[j] << "\t";
+            v += line[j];
+	}
+	std::cerr << v << std::endl;
+        //std::cerr << h_mean[i] << "\t";
+    }
 
     cudaFree(d_mean);
+    cudaCheckError();
     cudaFree(d_pT);
-    free(h_mean);
+    cudaCheckError();
+    //free(h_mean);
     free(h_p);
     //free(h_pT);
     return EXIT_SUCCESS;
