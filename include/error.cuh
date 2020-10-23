41882bb412a031254627eebdc6e4d6a62abd7718
Geoffrey Bossut <geoffrey.bossut@r01p12.sm14.sm.cri.epita.net>
2020-10-23 12:14:04 +0200

    GPUalg: Now working compilation with testlibgpualg files (each needs to be added to CMake...) and renamed sources

--- include/error.cuh
+++ include/error.cuh
@@ -7,3 +7,4 @@
         exit(EXIT_FAILURE);                                                                  \
     }                                                                                        \
 }
+
