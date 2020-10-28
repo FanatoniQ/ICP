2eb4d7fe16ee3c563a0eeb306c515e96d21a9eed
Tom Mechineau <tom.mechineau@gmail.com>
2020-10-28 13:45:04 +0100

    Add cpu SVD comparison

--- src/testlibgpualg/testgpusvd.cu
+++ src/testlibgpualg/testgpusvd.cu
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <tuple>
 #include "libCSV/csv.hpp"
+#include "libalg/CPUMatrix.hpp"
 
 int main(int argc, char** argv) 
 {
@@ -13,11 +14,19 @@ int main(int argc, char** argv)
 
 	size_t Pdim0, Pdim1;
 	std::string h{};
-	double* P = readCSV(argv[1], h, Pdim0, Pdim1);
+	double* array = readCSV(argv[1], h, Pdim0, Pdim1);
+	auto P = CPUMatrix{ array, Pdim0, Pdim1 };
 
-	auto [U, S, V_T] = svd(P, Pdim0, Pdim1);
+	auto [U, S, V_T] = svd(array, Pdim0, Pdim1);
 
-	std::cout << "Done!";
+	std::cout << "Done GPU!";
+
+	auto [V_T_cpu, S_cpu, U_cpu] = P.svd();
+
+	std::cout << "U matrix CPU:" << U_cpu;
+	std::cout << "S matrix CPU:" << S_cpu;
+	std::cout << "VT matrix CPU:" << V_T_cpu;
+
 	free(U);
 	free(S);
 	free(V_T);
