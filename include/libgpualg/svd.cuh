c59e9ca0a8f8767077647e7da866674177370af7
Tom Mechineau <tom.mechineau@gmail.com>
2020-10-28 19:35:45 +0100

    Better SVD tests and comments

--- include/libgpualg/svd.cuh
+++ include/libgpualg/svd.cuh
@@ -4,5 +4,12 @@
 * Performs an SVD calculation
 * Return U, S, VT in that order
 * Pointers will need to be freed
+* 
+* Exit dimensions!
+* m = row_A, n = col_A
+* (row * col)
+* U (m * m)
+* S (n)
+* VT (m * n)
 **/
 std::tuple<double*, double*, double*> svd(double* A, size_t r_A, size_t c_A);
--- src/testlibgpualg/testgpusvd.cu
+++ src/testlibgpualg/testgpusvd.cu
@@ -8,6 +8,19 @@
 #include "libCSV/csv.hpp"
 #include "libalg/CPUMatrix.hpp"
 
+int checkMatrix(double *A, double *B, size_t dim0, size_t dim1)
+{
+	for (int i = 0; i < dim0 * dim1; ++i)
+	{
+		if (std::abs(A[i] - B[i]) > 0.01)
+		{
+			std::cout << "Error at indice: " << i << "; Element " << A[i] << " is different from " << B[i] << std::endl;
+			return 0;
+		}
+	}
+	return 1;
+}
+
 int main(int argc, char** argv) 
 {
 	runtime_assert(argc == 2, "Usage: ./testgpusum file1");
@@ -17,6 +30,11 @@ int main(int argc, char** argv)
 	double* array = readCSV(argv[1], h, Pdim0, Pdim1);
 	auto P = CPUMatrix{ array, Pdim0, Pdim1 };
 
+	// m = row_A, n = col_A
+	// (row * col)
+	// U (m * m)
+	// S (n)
+	// VT (m * n)
 	auto [U, S, V_T] = svd(array, Pdim0, Pdim1);
 
 	std::cout << "Done GPU!";
@@ -26,6 +44,10 @@ int main(int argc, char** argv)
 	std::cout << "U matrix CPU:" << U_cpu;
 	std::cout << "S matrix CPU:" << S_cpu;
 	std::cout << "VT matrix CPU:" << V_T_cpu;
+
+	runtime_assert(checkMatrix(U, U_cpu.getArray(), Pdim0, Pdim0), "Check matrix U failed \n");
+	runtime_assert(checkMatrix(S, S_cpu.getArray(), Pdim1, 1), "Check matrix S failed \n");
+	runtime_assert(checkMatrix(V_T, V_T_cpu.getArray(), Pdim0, Pdim1), "Check matrix VT failed \n");
 
 	free(U);
 	free(S);
