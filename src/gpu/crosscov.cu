c08a6607d52f6f4167e08dcf3db53b8cb90eb87c
Geoffrey BOSSUT <geoffrey.bossut@epita.fr>
2020-10-31 00:16:18 +0100

    [GPU] ICP cross cov (#34)
    * GPU-cross-cov: Crosscov implementation kernel, need reduce sum axis=0 and add to cov
    
    * GPU-cross_cov: fix for Q_point.dot(P_point.T)
    
    * GPU-corresp: name fix
    
    * GPU-Cross-covs: testing for cross-covs
    
    * GPU-cross-covs: testing: now compiling but too much size for kernel arguments
    
    * GPU-cross-covs: investigating... cpu dot seems weird
    
    * GPU-test-cross-covs: errors git status
    
    * GPU-cross-covs: fixed crosscovs testing
    
    * GPU-cross-covs: lowering number of threads per block for testing
    
    * GPU-cross-covs: fixed, but weird h_ref we might have overmalloced
    
    * GPU-cross-cov: fix for pointer arithmetic logic
    
    Co-authored-by: Geoffrey Bossut <geoffrey.bossut@r01p11.sm14.sm.cri.epita.net>

---
+++ src/gpu/crosscov.cu
@@ -0,0 +1,91 @@
+#include <limits>
+#include <float.h>
+#include <assert.h>
+#include <stdio.h>
+#include <iostream>
+
+#include "libgpualg/mean.cuh"
+#include "gpu/crosscov.cuh"
+#include "error.cuh"
+#include "error.hpp"
+
+__global__ void get_cross_cov_kernel(const double *d_P, const double *d_Q, double *d_R, const ICPCorresp *d_dist,
+    size_t p_0, size_t p_1, size_t p_pitch,
+    size_t q_0, size_t q_1, size_t q_pitch,
+    size_t r_0, size_t r_1, size_t r_pitch,
+    size_t dist_0, size_t dist_1, size_t dist_pitch)
+{
+    assert(blockIdx.y == 0 && threadIdx.y == 0); // we are in 1d
+    //unsigned int colid = blockIdx.y * blockDim.y + threadIdx.y; // y index (unused)
+    unsigned int lineid = blockIdx.x * blockDim.x + threadIdx.x; // P index
+    if (lineid >= dist_0) {
+        return;
+    }
+    ICPCorresp *d_distline = (ICPCorresp *)((char*)d_dist + lineid * dist_pitch);
+    unsigned int idp = lineid; // p_point id
+    unsigned int idq = d_distline[0].id; // q_point id
+    // if pitch not in bytes:
+    //unsigned int idq = d_dist[lineid * dist_pitch].id;//d_distline[0].id; // q_point id
+
+    double *d_rline = (double *)((char *)d_R + idp * r_pitch);
+    double *d_pline = (double *)((char *)d_P + idp * p_pitch);
+    double *d_qline = (double *)((char *)d_Q + idq * q_pitch);
+    /**
+    // generic case:
+    //fake transpose dot between P and Q:
+    //d_rline stores P.dot(Q.T) flattened in the line
+    for (size_t q_i = 0; q_i < q_1 ; ++q_i)
+    {
+        for (size_t p_i = 0; p_i < p_1 ; ++p_i)
+        {
+            d_rline[p_i * q_1 + p_j] = d_qline[q_i] * d_pline[p_i];
+        }
+    }
+    **/
+    printf("%u -> %u\n", idp, idq);
+    //printf("P{%u:} %lf %lf %lf, Q{%u:} %lf %lf %lf \n", idp, d_pline[0], d_pline[1], d_pline[2], idq, d_qline[0], d_qline[1], d_qline[2]);
+    // loop unrolled version for dim3:
+    assert(p_1 == q_1 && p_1 == 3 && "Invalid: only dim3 is supported !");
+    d_rline[0] = d_qline[0] * d_pline[0];
+    d_rline[1] = d_qline[0] * d_pline[1];
+    d_rline[2] = d_qline[0] * d_pline[2];
+
+    d_rline[3] = d_qline[1] * d_pline[0];
+    d_rline[4] = d_qline[1] * d_pline[1];
+    d_rline[5] = d_qline[1] * d_pline[2];
+
+    d_rline[6] = d_qline[2] * d_pline[0];
+    d_rline[7] = d_qline[2] * d_pline[1];
+    d_rline[8] = d_qline[2] * d_pline[2];
+    //printf("%u %u: %lf %lf %lf | %lf %lf %lf | %lf %lf %lf\n", idp, idq, d_rline[0], d_rline[1], d_rline[2], d_rline[3], d_rline[4], d_rline[5], d_rline[6], d_rline[7], d_rline[8]);
+    //printf("P{%u:} %lf %lf %lf, Q{%u:} %lf %lf %lf \n", idp, d_pline[0], d_pline[1], d_pline[2], idq, d_qline[0], d_qline[1], d_qline[2]);
+}
+
+__host__ void get_cross_cov(const double *d_P, const double *d_Q, double **d_R, const ICPCorresp *d_dist,
+    size_t p_0, size_t p_1, size_t p_pitch,
+    size_t q_0, size_t q_1, size_t q_pitch,
+    size_t r_0, size_t r_1, size_t *r_pitch,
+    size_t dist_0, size_t dist_1, size_t dist_pitch, bool sync)
+{
+    runtime_assert(p_1 == q_1 && dist_1 == q_0 && dist_0 == p_0, "Invalid shapes !");
+    if (*d_R == nullptr)
+    {
+        cudaMallocPitch(d_R, r_pitch, p_1 * q_1 * sizeof(double), dist_0);
+        cudaCheckError();
+    }
+
+    dim3 blocksize(8, 1); // TODO: change nb threads
+    dim3 gridsize(std::ceil((float)dist_0 / blocksize.x), 1);
+    std::cerr << blocksize.x << " èè " << gridsize.x << std::endl;
+    
+    get_cross_cov_kernel<<<gridsize, blocksize>>>(d_P, d_Q, *d_R, d_dist,
+        p_0, p_1, p_pitch,
+        q_0, q_1, q_pitch,
+        r_0, r_1, *r_pitch,
+        dist_0, dist_1, dist_pitch);
+    
+    if (sync) {
+        cudaDeviceSynchronize();
+        cudaCheckError();
+    }
+}
---
+++ src/gpu/testcorresp.cu
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <tuple>
+#include <iostream>
+#include <limits>
+#include <float.h>
+
+// CPU
+#include "libCSV/csv.hpp"
+#include "libalg/CPUMatrix.hpp"
+#include "libalg/alg.hpp"
+#include "libalg/print.hpp"
+#include "error.hpp"
+
+
+// GPU
+#include "libgpualg/mean.cuh"
+#include "error.cuh"
+#include "gpu/icp.cuh"
+#include "gpu/corresp.cuh"
+
+double randomdouble(double low, double high)
+{
+    return ((double)rand() * (high - low)) / (double)RAND_MAX + low;
+}
+
+#ifdef TOTO
+int main(int argc, char **argv)
+{
+    srand(time(NULL));
+    // simulating P matrix with 10 lines (points) and Q matrix with 5 lines (points) correspondences
+    size_t dim0 = 100;
+    size_t dim1 = 3;
+    ICPCorresp *C = (ICPCorresp *)malloc(dim0 * dim1 * sizeof(ICPCorresp));
+    ICPCorresp *Cmin = (ICPCorresp *)malloc(dim0 * dim1 * sizeof(ICPCorresp));
+    for (size_t i = 0; i < dim0; ++i)
+    {
+	size_t minid;
+	double min = DBL_MAX;
+        for (size_t j = 0; j < dim1; ++j)
+        {
+            C[i * dim1 + j] = {randomdouble(0.0, 10.0), (unsigned int)j};
+	    if (C[i * dim1 + j].dist < min) {
+                 min = C[i * dim1 + j].dist;
+		 minid = j;
+            }
+            std::cerr << "dist: " << C[i * dim1 + j].dist << " id: " << C[i * dim1 + j].id << "\t";
+        }
+	Cmin[i * dim1] = {min, (unsigned int)minid};
+	std::cerr << std::endl;
+    }
+
+    size_t reducepitch;
+    ICPCorresp *d_C;
+    cudaMallocPitch((void **)&d_C, &reducepitch, dim1 * sizeof(ICPCorresp), dim0); // FIXME: crashes...
+    cudaCheckError();
+    cudaMemcpy2D(d_C, reducepitch, C, dim1 * sizeof(ICPCorresp), dim1 * sizeof(ICPCorresp), dim0, cudaMemcpyHostToDevice);
+    cudaCheckError();
+
+    get_correspondences(d_C, reducepitch, dim0, dim1, true);
+    std::cerr << "DONE" << std::endl;
+
+    ICPCorresp *h_res = (ICPCorresp *)malloc(dim0 * dim1 * sizeof(ICPCorresp));
+    cudaMemcpy2D(h_res, dim1 * sizeof(ICPCorresp), d_C, reducepitch, 1 * sizeof(ICPCorresp), dim0, cudaMemcpyDeviceToHost);
+    cudaCheckError();
+
+    std::cerr << "Min dist: " << std::endl;
+    size_t errors = 0;
+    for (size_t i = 0; i < dim0; ++i)
+    {
+        std::cerr << "dist: " << h_res[i * dim1].dist << " id: " << h_res[i * dim1].id << std::endl;
+	if (h_res[i * dim1].id != Cmin[i * dim1].id)
+	{
+             errors++;
+             std::cerr << "DIFFERENCE ! " << std::endl << "refdist: " << Cmin[i * dim1].dist << " refid: " << Cmin[i * dim1].id << std::endl;
+	}
+    }
+    std::cerr << "ERRORS:" << errors << std::endl;
+    free(h_res);
+    free(C);
+    cudaFree(d_C);
+    cudaCheckError();
+}
+#endif
