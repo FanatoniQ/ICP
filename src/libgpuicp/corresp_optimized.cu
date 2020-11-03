748c2b33e2cda9cc4ea0bb30a39cb4a3c9d41745
Geoffrey Bossut <geoffrey.bossut@r01p11.sm14.sm.cri.epita.net>
2020-11-03 18:53:24 +0100

    get_array_correspondences_optimized_kernel now with no bank conflicts

--- src/libgpuicp/corresp_optimized.cu
+++ src/libgpuicp/corresp_optimized.cu
@@ -28,9 +28,14 @@ __global__ void get_array_correspondences_optimized_kernel(unsigned int *d_array
     unsigned int nbiters)
 {
     assert(P_col == Q_col && P_col == 3);
-    extern __shared__ double s_data[]; // first 3 * sizeof(double) bytes are used to store p_point[0,1,2], then we have 1025 ICPCorresps
-    ICPCorresp *s_min_point = (ICPCorresp*)(s_data + 3); // last is final min
-    ICPCorresp *s_corresp = s_min_point + 1; // pointer to ICPCorresp structure
+    //extern __shared__ double s_data[]; // first 3 * sizeof(double) bytes are used to store p_point[0,1,2], then we have 1025 ICPCorresps
+    extern __shared__ double s_corresp[];
+    
+    double *s_dists = s_corresp;
+    unsigned int *s_ids = (unsigned int *)(s_dists + blockDim.x);
+    double *s_min_dist = (double *)(s_ids + blockDim.x);
+    unsigned int *s_min_id = (unsigned int *)(s_min_dist + 1);
+
     unsigned int qid = threadIdx.x; //blockIdx.x * blockDim.x + threadIdx.x;
     unsigned int pid = blockIdx.x; // * blockDim.x + threadIdx.y; // blockDim.y == 1 and blockDim.x == 1024
     double dist, tmp;
@@ -39,29 +44,30 @@ __global__ void get_array_correspondences_optimized_kernel(unsigned int *d_array
     unsigned int iter = 0;
     if (qid == 0)
     {
-        // storing p_point x, y and z for all threads in block
-        s_data[0] = p_point[0];
-        s_data[1] = p_point[1];
-        s_data[2] = p_point[2];
-        s_min_point[0] = { DBL_MAX, 0 };
+        //s_min_point[0] = { DBL_MAX, 0 };
+	s_min_dist[0] = DBL_MAX;
+	//s_min_id[0] = 0;
     }
     __syncthreads(); // wait for x,y and z of p_point (should be done at first iter only)
     do {
         if (qid >= Q_row) {
-            s_corresp[threadIdx.x] = { DBL_MAX, 0 };
+	    s_dists[threadIdx.x] = DBL_MAX;
+            //s_corresp[threadIdx.x] = { DBL_MAX, 0 };
             return;
         }
         q_point = d_Q + qid * Q_col;
         // compute distance for qid
         dist = 0;
-        tmp = s_data[0] - q_point[0];
+        tmp = p_point[0] - q_point[0];
         dist += tmp * tmp;
-        tmp = s_data[1] - q_point[1];
+        tmp = p_point[1] - q_point[1];
         dist += tmp * tmp;
-        tmp = s_data[2] - q_point[2];
+        tmp = p_point[2] - q_point[2];
         dist += tmp * tmp;
         // store the distance
-        s_corresp[threadIdx.x] = { dist, qid };
+        //s_corresp[threadIdx.x] = { dist, qid };
+	s_dists[threadIdx.x] = dist;
+	s_ids[threadIdx.x] = qid;
         __syncthreads();
         // min dist with friends
         assert(is_power_of_2(blockDim.x / 2));
@@ -69,22 +75,35 @@ __global__ void get_array_correspondences_optimized_kernel(unsigned int *d_array
         for(unsigned int stride = blockDim.x / 2; stride > 0; stride >>= 1)
         {
             if (threadIdx.x < stride) {
+		    /**
                 if (s_corresp[threadIdx.x].dist > s_corresp[threadIdx.x + stride].dist)
                     s_corresp[threadIdx.x] = s_corresp[threadIdx.x + stride];
+		    **/
+		if (s_dists[threadIdx.x] > s_dists[threadIdx.x + stride])
+		{
+s_dists[threadIdx.x] = s_dists[threadIdx.x + stride];
+s_ids[threadIdx.x] = s_ids[threadIdx.x + stride];
+		}
             }
             __syncthreads();
         }
         // total min dist
         if (threadIdx.x == 0)
         {
+		/**
             if (s_min_point[0].dist > s_corresp[0].dist)
                 s_min_point[0] = s_corresp[0];
+		**/
+	    if (s_min_dist[0] > s_dists[0]) {
+		s_min_dist[0] = s_dists[0];
+		s_min_id[0] = s_ids[0];
+	    } 
         }
         // increase qid
         qid += blockDim.x; // += 1024
     } while(++iter < nbiters);
     if (threadIdx.x == 0)
-        d_array_correspondances[pid] = s_min_point[0].id;
+        d_array_correspondances[pid] = s_min_id[0];//.id;
 }
 
 __host__ void get_array_correspondences_optimized(unsigned int* d_array_correspondances, const double *d_P, const double *d_Q,
@@ -98,7 +117,7 @@ __host__ void get_array_correspondences_optimized(unsigned int* d_array_correspo
     std::cerr << "blocksize.x: " << blocksize.x << std::endl;
     std::cerr << "nbiters:" << nbiters << std::endl;
 
-    get_array_correspondences_optimized_kernel<<<gridsize, blocksize, sizeof(ICPCorresp) * blocksize.x + sizeof(ICPCorresp) + 3 * sizeof(double)>>>(d_array_correspondances,
+    get_array_correspondences_optimized_kernel<<<gridsize, blocksize, sizeof(ICPCorresp) * blocksize.x + sizeof(ICPCorresp)>>>(d_array_correspondances,
         d_P, d_Q, P_row, P_col, Q_row, Q_col, nbiters);
     cudaDeviceSynchronize();
     cudaCheckError();
