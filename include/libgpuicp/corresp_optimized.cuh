b73e68fa1138b75b9c06a20ecf767606a72cc81b
Geoffrey Bossut <geoffrey.bossut@epita.fr>
2020-11-03 01:13:03 +0100

    One iter with final reduce for d_array_correspondances (experimental)

--- include/libgpuicp/corresp_optimized.cuh
+++ include/libgpuicp/corresp_optimized.cuh
@@ -23,8 +23,16 @@ __global__ void get_array_correspondences_optimized_kernel(unsigned int *d_array
 __host__ void get_array_correspondences_optimized(unsigned int* d_array_correspondances, const double *d_P, const double *d_Q,
     unsigned int P_row, unsigned int P_col, unsigned int Q_row, unsigned int Q_col);
 
+ //////////////
+// ONE ITER //
+/////////////
+
 __global__ void get_array_correspondences_optimized_one_iter_kernel(ICPCorresp *d_dists, unsigned int dist_1,
     const double *d_P, const double *d_Q, unsigned int P_row, unsigned int P_col, unsigned int Q_row, unsigned int Q_col);
 
-__host__ void get_array_correspondences_optimized_one_iter(ICPCorresp **d_dist, unsigned int *dist_1, const double *d_P, const double *d_Q,
+__global__ void get_array_reduced_correspondences_kernel(unsigned int *d_array_correspondances, ICPCorresp *d_dist,
+    size_t dist_pitch, size_t dist_0, size_t dist_1)
+
+__host__ void get_array_correspondences_optimized_one_iter(unsigned int *d_array_correspondances,
+    ICPCorresp **d_dist, unsigned int *dist_1, const double *d_P, const double *d_Q,
     unsigned int P_row, unsigned int P_col, unsigned int Q_row, unsigned int Q_col);
